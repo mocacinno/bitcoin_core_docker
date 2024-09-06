42881333d79eb0059bd3eff46391bfce25eca30e
mocacinno <mocacinno@gmx.com>
2024-09-06 08:47:30 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-mkdir -p /root/.bitcoin/
-
-echo "rpcuser=bitcoinrpc" > /root/.bitcoin/bitcoin.conf
-echo "rpcpassword=changeme" >> /root/.bitcoin/bitcoin.conf
-
 bitcoind -daemon
 
 sleep 10
