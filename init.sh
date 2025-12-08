ba94e699704be98a079ca8d5073423524435f905
mocacinno <mocacinno@gmx.com>
2025-12-08 10:53:28 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 set -e
 CONF_DIR="/home/bitcoinuser/.bitcoin"
 mkdir -p "$CONF_DIR"
-RPC_PASSWORD=$(openssl rand -hex 16)
+RPC_PASSWORD=$(head -c 18 /dev/urandom | base64 | tr -d '=+/')
 cat > "$CONF_DIR/bitcoin.conf" <<EOF
 rpcuser=bitcoinrpc
 rpcpassword=$RPC_PASSWORD
